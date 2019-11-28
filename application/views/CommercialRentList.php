f9552596b6af58e578edda9012cccd8b93138922
ParmodCZ <pramod.kumar@contriverz.com>
2019-11-28 20:42:32 +0530

    gdgdgdgcc

--- application/views/CommercialRentList.php
+++ application/views/CommercialRentList.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-xs-12 text-right">
                 <div class="form-group">
-                    <a class="btn btn-primary" href="<?php echo base_url(); ?>AddResidentialResaleProperty"><i class="fa fa-plus"></i> Add New</a>
+                    <a class="btn btn-primary" href="<?php echo base_url(); ?>addNewCommercialRent"><i class="fa fa-plus"></i> Add New</a>
                 </div>
             </div>
         </div>
@@ -20,7 +20,7 @@
                 <div class="box-header">
                     <h3 class="box-title">Users List</h3>
                     <div class="box-tools">
-                        <form action="<?php echo base_url() ?>userListing" method="POST" id="searchList">
+                        <form action="<?php echo base_url() ?>CommercialRentList" method="POST" id="searchList">
                             <div class="input-group">
                               <input type="text" name="searchText" value="<?php echo $searchText; ?>" class="form-control input-sm pull-right" style="width: 150px;" placeholder="Search"/>
                               <div class="input-group-btn">
@@ -33,11 +33,11 @@
                 <div class="box-body table-responsive no-padding">
                   <table class="table table-hover">
                     <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Mobile</th>
-                        <th>Role</th>
-                        <th>Created On</th>
+                        <th>Property Type</th>
+                        <th>Area</th>
+                        <th>Landmark</th>
+                        <th>Floor Info</th>
+                        <th>City</th>
                         <th class="text-center">Actions</th>
                     </tr>
                     <?php
@@ -53,8 +53,8 @@
                         <td><?php echo $record->floor_info ?></td>
                         <td><?php echo $record->city ?></td>
                         <td class="text-center">
-                            <a class="btn btn-sm btn-info" href="<?php echo base_url().'editResidentialResaleProperty/'.$record->propertyid; ?>" title="Edit"><i class="fa fa-pencil"></i></a>
-                            <a class="btn btn-sm btn-danger deleteResidentialResaleProperty" href="#" data-propertyid="<?php echo $record->propertyid; ?>" title="Delete"><i class="fa fa-trash"></i></a>
+                            <a class="btn btn-sm btn-info" href="<?php echo base_url().'editCommercialRentProperty/'.$record->propertyid; ?>" title="Edit"><i class="fa fa-pencil"></i></a>
+                            <a class="btn btn-sm btn-danger" href="#" onclick="deletefn(<?php echo $record->propertyid; ?>,'deleteCommercialRentProperty',' Commercial Rent Property')" title="Delete"><i class="fa fa-trash"></i></a>
                         </td>
                     </tr>
                     <?php
--- assets/js/common.js
+++ assets/js/common.js
@@ -81,29 +81,28 @@ jQuery(document).ready(function(){
 	jQuery(document).on("click", ".searchList", function(){
 		
 	});
-	
-	deletefun(moduleid,url,module='this'){
-		var propertyid = moduleid,
-			hitURL = baseURL + url,
-			currentRow = $(this);
-		
-		var confirmation = confirm("Are you sure to delete this Property ?");
-		
-		if(confirmation)
-		{
-			jQuery.ajax({
-			type : "POST",
-			dataType : "json",
-			url : hitURL,
-			data : { propertyid : propertyid } 
-			}).done(function(data){
-				console.log(data);
-				currentRow.parents('tr').remove();
-				if(data.status = true) { alert("Property successfully deleted"); }
-				else if(data.status = false) { alert("Property deletion failed"); }
-				else { alert("Access denied..!"); }
-			});
-		}
-	}
-	
 });
+
+function deletefn(moduleid,url,module='this'){
+	var propertyid = moduleid,
+		hitURL = baseURL + url,
+		currentRow = $(this);
+	
+	var confirmation = confirm("Are you sure to delete this Property ?");
+	
+	if(confirmation)
+	{
+		jQuery.ajax({
+		type : "POST",
+		dataType : "json",
+		url : hitURL,
+		data : { propertyid : propertyid } 
+		}).done(function(data){
+			console.log(data);
+			currentRow.parents('tr').remove();
+			if(data.status = true) { alert("Property successfully deleted"); }
+			else if(data.status = false) { alert("Property deletion failed"); }
+			else { alert("Access denied..!"); }
+		});
+	}
+}
